0f076fcfcb44c0c358c56ee6e9addba33c28406c
枯叶_听海 <5490584810@163.com>
2017-02-23 19:56:09 +0800

    简单事务传递处理

--- src/main/java/com/jeremie/thread/DynamicService.java
+++ src/main/java/com/jeremie/thread/DynamicService.java
@@ -4,12 +4,14 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
+import java.util.Stack;
 
 /**
  * @author guanhong 2017/2/22.
  */
 public class DynamicService implements MethodInterceptor {
 
+    private static ThreadLocal<Stack<Method>> methodStack = ThreadLocal.withInitial(Stack::new);
 
     private Service service;
 
@@ -18,26 +20,35 @@ public class DynamicService implements MethodInterceptor {
         this.service.setDao(ApplicationContext.dao);
     }
 
-    private void before() {
-        ApplicationContext.connectionThreadLocal.get().startTransaction();
+    private void before(Method method) {
+        if (methodStack.get().isEmpty()) {
+            ApplicationContext.connectionThreadLocal.get().startTransaction();
+        }
+        methodStack.get().push(method);
     }
 
     private void after() {
-        ApplicationContext.connectionThreadLocal.get().commit();
-        ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
-        ApplicationContext.connectionThreadLocal.remove();
+        methodStack.get().pop();
+        if (methodStack.get().empty()) {
+            ApplicationContext.connectionThreadLocal.get().commit();
+            ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
+            ApplicationContext.connectionThreadLocal.remove();
+        }
     }
 
     private void exception(Exception e) throws Exception {
         e.printStackTrace();
-        ApplicationContext.connectionThreadLocal.get().rollBack();
-        ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
-        ApplicationContext.connectionThreadLocal.remove();
+        methodStack.get().pop();
+        if (methodStack.get().empty()) {
+            ApplicationContext.connectionThreadLocal.get().rollBack();
+            ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
+            ApplicationContext.connectionThreadLocal.remove();
+        }
     }
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        this.before();
+        this.before(method);
         Object result;
         try {
             result = method.invoke(this.service, objects);
