70e55d49388a37e216a069710991d5405c06599c
枯叶_听海 <5490584810@163.com>
2017-02-24 09:54:36 +0800

    BaseDynamicService

--- src/main/java/com/jeremie/thread/DynamicService.java
+++ src/main/java/com/jeremie/thread/DynamicService.java
@@ -4,46 +4,18 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
-import java.util.Stack;
 
 /**
  * @author guanhong 2017/2/22.
  */
-public class DynamicService implements MethodInterceptor {
+public class DynamicService extends BaseDynamicService implements MethodInterceptor {
 
-    private static ThreadLocal<Stack<Method>> methodStack = ThreadLocal.withInitial(Stack::new);
 
     private Service service;
 
     DynamicService(Service service) {
         this.service = service;
         this.service.setDao(ApplicationContext.dao);
-    }
-
-    private void before(Method method) {
-        if (methodStack.get().isEmpty()) {
-            ApplicationContext.connectionThreadLocal.get().startTransaction();
-        }
-        methodStack.get().push(method);
-    }
-
-    private void after() {
-        methodStack.get().pop();
-        if (methodStack.get().empty()) {
-            ApplicationContext.connectionThreadLocal.get().commit();
-            ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
-            ApplicationContext.connectionThreadLocal.remove();
-        }
-    }
-
-    private void exception(Exception e) throws Exception {
-        e.printStackTrace();
-        methodStack.get().pop();
-        if (methodStack.get().empty()) {
-            ApplicationContext.connectionThreadLocal.get().rollBack();
-            ApplicationContext.connectionPool.releaseConnection(ApplicationContext.connectionThreadLocal.get().getId());
-            ApplicationContext.connectionThreadLocal.remove();
-        }
     }
 
     @Override
