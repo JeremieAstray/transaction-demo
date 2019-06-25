35984a5c321d9f8d2ab18a59acc2018e3758535f
枯叶_听海 <5490584810@163.com>
2019-06-25 23:48:37 +0800

    稍微加了点注释

--- src/main/java/com/jeremie/asm/Test.java
+++ src/main/java/com/jeremie/asm/Test.java
@@ -3,12 +3,17 @@ package com.jeremie.asm;
 import org.objectweb.asm.*;
 import org.objectweb.asm.commons.AdviceAdapter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.lang.reflect.Method;
 
 /**
  * @author guanhong 2019-06-25.
  */
 public class Test {
+
+    private final static String CLAZZ_NAME = "com.jeremie.asm.AsmTest";
 
     public static void main(String[] args) throws Exception {
         AsmTest asmTest = new AsmTest();
@@ -18,23 +23,36 @@ public class Test {
         asmTest.addAge();
         System.out.println(asmTest.getAll());
 
+        //读取类
+        ClassReader classReader = new ClassReader(CLAZZ_NAME);
 
-        String clazzName = "com.jeremie.asm.AsmTest";
+        //字节码编辑器
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
 
-        final int flag = ClassWriter.COMPUTE_MAXS;
-        ClassReader classReader = new ClassReader(clazzName);
-        ClassWriter classWriter = new ClassWriter(flag);
+        //字节码访问器
         ClassVisitor change = new ChangeVisitor(classWriter);
+
+        //访问者模式，加载ClassVisitor
         classReader.accept(change, ClassReader.EXPAND_FRAMES);
 
-        Class clazz = new MyClassLoader().defineClass(clazzName, classWriter.toByteArray());
+        //用新的字节码生成类
+        Class clazz = new MyClassLoader().defineClass(CLAZZ_NAME, classWriter.toByteArray());
+        File newClazz = new File("D:\\temporary\\programme\\IdeaProjects\\transaction-demo\\target\\classes\\com\\jeremie\\asm", "AsmTestNew.class");
+        FileOutputStream fileOutputStream = new FileOutputStream(newClazz);
+        fileOutputStream.write(classWriter.toByteArray());
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        //实例化类
         Object personObj = clazz.newInstance();
         System.out.println("init Success!");
         System.out.println();
+
+        //反射调用生成类的addAge方法
         Method nameMethod = clazz.getDeclaredMethod("addAge", null);
         nameMethod.invoke(personObj, null);
         System.out.println();
 
+        //反射调用生成类的getAge方法
         Method getMethod = clazz.getDeclaredMethod("getAge", null);
         Object result = getMethod.invoke(personObj, null);
         System.out.println(result);
@@ -58,7 +76,7 @@ public class Test {
     }
 
     static class ChangeAdapter extends AdviceAdapter {
-        private int startTimeId = -1;
+        //private int startTimeId = -1;
         private String methodName = null;
 
         ChangeAdapter(int api, MethodVisitor mv, int access, String name, String desc) {
@@ -67,17 +85,11 @@ public class Test {
             //startTimeId = newLocal(Type.LONG_TYPE);
             //mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
             //mv.visitIntInsn(LSTORE, startTimeId);
-            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-            mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
-            mv.visitInsn(DUP);
-            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
-            mv.visitLdcInsn("The method " + methodName + " is start ");
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
-            //mv.visitVarInsn(LLOAD, startTimeId);
-            //mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+
+            //方法开始前往字节码中插入一段输出方法开始的代码
+            addMethodFlag("start");
         }
+
 
         @Override
         protected void onMethodExit(int opcode) {
@@ -89,6 +101,13 @@ public class Test {
                 mv.visitInsn(IADD);
                 mv.visitFieldInsn(PUTFIELD, "V", "age", Type.getDescriptor(int.class));
             }*/
+
+            //方法结束前往字节码中插入一段输出方法结束的代码
+            addMethodFlag("end");
+
+        }
+
+        private void addMethodFlag(String flag) {
             int durationId = newLocal(Type.LONG_TYPE);
             /*mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
             mv.visitVarInsn(LLOAD, startTimeId);
@@ -98,7 +117,7 @@ public class Test {
             mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
             mv.visitInsn(DUP);
             mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
-            mv.visitLdcInsn("The method " + methodName + " is end ");
+            mv.visitLdcInsn("The method " + methodName + " is " + flag);
             mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
             //mv.visitVarInsn(LLOAD, startTimeId);
             //mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
